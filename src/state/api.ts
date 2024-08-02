6e1431b2229ae8f3e9a8747ce8c33129813a02db
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-08-02 13:40:37 +0300

    done

--- src/state/api.ts
+++ src/state/api.ts
@@ -1,11 +1,11 @@
-import { createApi } from "@reduxjs/toolkit/query";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
 
 
 export const api = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
     reducerPath: "api",
     tagTypes: [],
-    endpoins: (build) => ({}),
+    endpoints: (build) => ({}),
 });
 
 export const {} = api;
